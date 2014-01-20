3302f44f14947e86415bc8281b442a634d2abba8
Marhaak <rodland92@hotmail.com>
2014-01-20 20:38:14 +0100

    hei

--- AI1/AI1/Environment.cpp
+++ AI1/AI1/Environment.cpp
@@ -2,10 +2,10 @@
 #include "Include.h"
 #include "Environment.h"
 
-<<<<<<< HEAD
+
 Environment::Environment(void)
 {
-=======
+
 
 
 Environment::Environment(int x, int y){
@@ -13,7 +13,7 @@ Environment::Environment(int x, int y){
 	map.resize( x, vector<int>(y, 0) );
 
 
->>>>>>> 7a1ae0086e9d1ba648cac314150b094485ce5266
+
 }
 
 
