658106b25c767092d6f08287f344d4d5dcd5067d
spoki0 <spoki0@hotmail.com>
2014-01-20 20:54:50 +0100

    eh

--- AI1/AI1/Environment.cpp
+++ AI1/AI1/Environment.cpp
@@ -8,11 +8,16 @@ Environment::Environment(int x, int y){
 
 	map.resize( x, std::vector<int>(y, 0) );
 
+	for (int i = 0; i < x; i++){
+		for (int j = 0; j < y; j++){
 
+		}
+	}
 
 }
 
 
-Environment::~Environment(void)
-{
+Environment::~Environment(void){
+
+
 }
