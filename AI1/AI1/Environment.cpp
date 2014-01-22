25ea51667c0321f7b4fe60a47725066798198432
spoki0 <spoki0@hotmail.com>
2014-01-22 15:25:52 +0100

    More Node stuff

--- AI1/AI1/Environment.cpp
+++ AI1/AI1/Environment.cpp
@@ -8,20 +8,23 @@
 Environment::Environment(int _x, int _y){
 
 	srand( time(NULL) );
-	map.resize( _x, std::vector<int>(_y, Node) );
+	
+	//que?
+	map.resize( _x, std::vector<int>(_y, 0) );
 
 	for (int i = 0; i < _x; i++){
 		for (int j = 0; j < _y; j++){
-			map[i][j].setValue( rand() % 3 );
+			map[i][j] = new Node;
+			map[i][j]->setValue( rand() % 3 );
 		}
 	}
 
 	for (int i = 0; i < _x; i++){
 		for (int j = 0; j < _y; j++){
 			
-			if (map[i][j].getValue() == 0){std::cout << "# ";}
-			if (map[i][j].getValue() == 1){std::cout << "- ";}
-			if (map[i][j].getValue() == 2){std::cout << "+ ";}
+			if (map[i][j]->getValue() == 0){std::cout << "# ";}
+			if (map[i][j]->getValue() == 1){std::cout << "- ";}
+			if (map[i][j]->getValue() == 2){std::cout << "+ ";}
 
 		}
 		std::cout << std::endl;
