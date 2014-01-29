9f4dfc2cc7360762113d49f3d457b80653ef3441
Marhaak <rodland92@hotmail.com>
2014-01-29 19:56:54 +0100

    Better moving logic

--- AI1/AI1/Agent.cpp
+++ AI1/AI1/Agent.cpp
@@ -22,6 +22,7 @@ Agent::~Agent(void){
 int Agent::Run(){
 
 	running = true;
+	Vacuum();
 	//infinite to 1k running loop :D
 	while (running) {
 		world->draw(posX, posY);
@@ -32,7 +33,9 @@ int Agent::Run(){
 		if(steps > 1000) {
 			running = false;
 		}
+		cin.get();
 		system("CLS");
+		
 	}
 	return 1;
 };
@@ -62,65 +65,93 @@ void Agent::Move() {
 
 	Sleep(1000);
 
-	// Down
-	if(world->isMoveAble(posX+1, posY)->getValue() != 2) {
-		
-		if(!world->isMoveAble(posX+1, posY)->getVisit() ) {
+	//// Down
+	//if(world->isMoveAble(posX+1, posY)->getValue() != 2) {
+	//	cout<< "Down\n";
+	//	if(!world->isMoveAble(posX+1, posY)->getVisit() ) {
+	//		posX += 1;
+	//		beenHere = false;
+	//		cout<< "Down not visited\n";
+	//	} else {
+	//		posXBeenHere += 1;
+	//		beenHere = true;
+	//	}
+	//}
+
+	//// right
+	//else if(world->isMoveAble(posX, posY+1)->getValue() != 2) {
+	//	cout<< "Right\n";
+	//	if(!world->isMoveAble(posX, posY+1)->getVisit() ) {
+	//		posY += 1;
+	//		beenHere = false;
+	//	} else {
+	//		posYBeenHere += 1;
+	//		beenHere = true;
+	//	}
+	//}
+
+	//// down
+	//else if(world->isMoveAble(posX-1, posY)->getValue() != 2) {
+	//	cout<< "Up\n";
+	//	if(!world->isMoveAble(posX-1, posY)->getVisit() ) {
+	//		posX -= 1;
+	//		beenHere = false;
+	//	} else {
+	//		posXBeenHere -= 1;
+	//		beenHere = true;
+	//	}
+	//}
+
+	//// left
+	//else if(!world->isMoveAble(posX, posY-1)->getValue() != 2) {
+	//	cout<< "Left\n";
+	//	if(world->isMoveAble(posX, posY-1)->getVisit() ) {
+	//	posY -= 1;
+	//	beenHere = false;
+	//	} else {
+	//		posYBeenHere -= 1;
+	//		beenHere = true;
+	//	}
+	//} 
+	////stuck
+	//else{
+	//	cout << "Trapped bot is unhappy" << std::endl;
+	//	running = false;
+	//}
+
+	srand( time(NULL) );
+	int randomz = rand() % 3;
+	if(randomz == 0) { // Down
+
+		if(world->isMoveAble(posX+1, posY)->getValue() != 2) {
 			posX += 1;
-			beenHere = false;
-		} else {
-			posXBeenHere += 1;
-			beenHere = true;
-		}
-	}
-
-	// right
-	else if(world->isMoveAble(posX, posY+1)->getValue() != 2) {
-
-		if(!world->isMoveAble(posX, posY+1)->getVisit() ) {
-			posY += 1;
-			beenHere = false;
-		} else {
-			posYBeenHere += 1;
-			beenHere = true;
-		}
-	}
-
-	// down
-	else if(world->isMoveAble(posX-1, posY)->getValue() != 2) {
-
-		if(!world->isMoveAble(posX-1, posY)->getVisit() ) {
-			posX -= 1;
-			beenHere = false;
-		} else {
-			posXBeenHere -= 1;
-			beenHere = true;
-		}
-	}
-
-	// left
-	else if(world->isMoveAble(posX, posY-1)->getValue() != 2) {
-		
-		if(!world->isMoveAble(posX, posY-1)->getVisit() ) {
-		posY -= 1;
-		beenHere = false;
-		} else {
-			posYBeenHere -= 1;
-			beenHere = true;
 		}
 	} 
-	//stuck
-	else{
-		cout << "Trapped bot is unhappy" << std::endl;
-		running = false;
+	else if(randomz == 1) { // Right
+
+		if(world->isMoveAble(posX, posY+1)->getValue() != 2) {
+			posY += 1;
+		}
+	}
+	else if(randomz == 2) { // Up
+
+		if(world->isMoveAble(posX-1, posY)->getValue() != 2) {
+			posX -= 1;
+		}
+	}
+	else if(randomz == 3) { // Up
+
+		if(world->isMoveAble(posX-1, posY)->getValue() != 2) {
+			posY -= 1;
+		}
 	}
 
 	//new position
-	if(!beenHere) {
+	/*if(!beenHere) {*/
 		positionNode = world->isMoveAble(posX, posY);
-	} else {
+	/*} else {
 		positionNode = world->isMoveAble(posXBeenHere, posYBeenHere);
-	}
+	}*/
 
 	positionNode->visit();
 	std::cout<< "Moved to x: "<<posX<< " y: "<< posY<<std::endl;
